0b51376d0ccf1f930faabce6edfa4e50558ecb2b
Sri Satya Prasad Adusumilli <satyaadusumilli@Sris-MacBook-Air.local>
2023-08-05 21:20:38 +0530

    code

---
+++ script.js
@@ -0,0 +1,24 @@
+const items = document.querySelectorAll(".item");
+
+      items.forEach((item) => {
+        const x = item.querySelector(".title");
+        const y = item.querySelector(".content");
+
+        x.addEventListener("click", () => {
+          item.classList.toggle("active");
+        });
+      });
+
+
+// document.title = "My Website";
+document.addEventListener("visibilitychange", function() 
+{
+if (document.hidden)
+{
+    document.title = "Hey, come back! : (";
+} 
+else 
+{
+    document.title = "CodeNinja ❤️";
+}
+});
